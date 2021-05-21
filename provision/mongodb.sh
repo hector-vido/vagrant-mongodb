51b7eecbd4e6689647cbc594e47f91a608761c47
Hector Vido <hector_vido@yahoo.com.br>
2021-05-21 15:32:35 -0300

    Adicionado lvm e xfsprogs

--- provision/mongodb.sh
+++ provision/mongodb.sh
@@ -17,7 +17,7 @@ if [ "$(swapon -v)" == '' ]; then
 fi
 
 apt-get update
-apt-get install -y vim wget gnupg git
+apt-get install -y vim wget gnupg git lvm2 xfsprogs
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 
 echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/4.4 main" > /etc/apt/sources.list.d/mongodb-org-4.4.list
